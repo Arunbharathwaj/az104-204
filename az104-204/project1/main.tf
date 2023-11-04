da61c562890f983f6b810611d03086f8b4a11503
Arunbharathwaj <aarunb06@gmail.com>
2023-11-04 09:40:55 +0800

    added project 1 entry in resources file and moved main.tf to project dir

---
+++ az104-204/project1/main.tf
@@ -0,0 +1,3 @@
+resource "instance" "instance" {
+  
+}
--- az104-204/project1/resource.tf
+++ az104-204/project1/resource.tf
@@ -1,4 +1,4 @@
-# Resource Block
+# Resource Block Project 1
 # Create a resource group
 resource "azurerm_resource_group" "Az200Group2" {
   name = "Az200Group2"
